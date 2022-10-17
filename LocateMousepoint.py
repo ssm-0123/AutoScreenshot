97f2c2728b5491d28bbed6666ee16b9ee47de389
ssm-0123 <90653235+ssm-0123@users.noreply.github.com>
2022-10-17 10:58:05 -0700

    o^o

---
+++ LocateMousepoint.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+
+time.sleep(1)
+for i in range(0,50):
+    time.sleep(0.1)
+    print(pyautogui.position())
---
+++ Readme.txt
@@ -0,0 +1,7 @@
+
+Before you run this program, you need to install two modules.
+
+pip install pyautogui
+
+pip install pillow
+
